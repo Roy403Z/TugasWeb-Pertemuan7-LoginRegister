3ec661255d63406aea5ca398253362e4e0a3c2f3
Ade Roy <aderoysimbolonr@gmail.com>
2026-09-25 22:49:58 +0700

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-$_SESSION = array();
-session_destroy();
+if (isset($_COOKIE['user_login_data'])) {
+    setcookie('user_login_data', '', time() - 3600, "/");
+}
 
 header('Location: login.php');
 exit();
